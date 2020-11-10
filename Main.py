e397104c967e01123d75eb26c9480ab70fb990ba
DarkManPC <guillaume.longrais@orange.fr>
2020-11-11 00:19:02 +0100

    j'ai les pairs

--- Main.py
+++ Main.py
@@ -11,13 +11,21 @@ def frequent(support):
     rawPurchases = sc.textFile("datas/T10I4D100K.dat")
     #print(rawPurchases.collect())
     total = rawPurchases.count()
-    words= rawPurchases.flatMap(lambda line: line.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
+    words = rawPurchases.flatMap(lambda line: line.split(" "))
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b: a + b)
     wordCounts = wordCounts.filter(lambda x: len(x[0])>=1 ) #Remove retour à la ligne
     wordCounts = wordCounts.filter(lambda x: x[1]/total >= support ) #Remove retour à la ligne
 
     purchases = rawPurchases.map(lambda x: x.split(" "))
-    print(purchases.collect())
+
+    pair = wordCounts.map(lambda x: x[0]) # get the item for the pair (item, count)
+    pair = pair.cartesian(pair) # Compute all the pairs of items possible
+    pair = pair.filter(lambda x: x[0] != x[1]) # Remove pairs (x, x)
+    pair = pair.map(lambda x: x if x[0] < x[1] else (x[1], x[0])) # Order elements in pair  : [(1,2), (2,1)] -> [(1,2), (1,2)]
+    pair = pair.distinct() # Remove duplicated pairs
+
+    print(wordCounts.collect())
+    print(pair.collect())
 
 
 
