8a69ffc90c5b49ab6d3d5f81ee403c267f6cb3ed
DarkManPC <guillaume.longrais@orange.fr>
2020-11-11 11:37:18 +0100

    finishing apriori for pairs

--- Main.py
+++ Main.py
@@ -9,26 +9,13 @@ import sys
 sc = SparkContext('local[*]')
 spark = SparkSession(sc)
 
-def containIn(v, rdd):
-    tmp = rdd.filter(lambda x: x==v)
-    return tmp.count() !=0
-
-def getPair(l):
-    tmp = sc.parralelize(l)
-    tmp = tmp.cartesian(tmp)
-    print(tmp.collect())
-    return tmp.collect()
-
 def frequent(support):
     rawPurchases = sc.textFile("datas/T10I4D100K1.dat")
-    #print(rawPurchases.collect())
     total = rawPurchases.count()
     words = rawPurchases.flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b: a + b)
     wordCounts = wordCounts.filter(lambda x: len(x[0])>=1 ) #Remove retour à la ligne
     wordCounts = wordCounts.filter(lambda x: (x[1]/total)*100 >= support ) #Remove retour à la ligne
-
-    #getPairUdf = udf(getPair, ArrayType(IntegerType()))
 
     purchases = rawPurchases.map(lambda x: x.split(" "))
 
@@ -40,9 +27,6 @@ def frequent(support):
         tmp = sc.parallelize(i)
         tmp = tmp.cartesian(tmp)
         pairCount = pairCount.union(tmp)
-
-    #TODO finish with list in RDD not RDD in RDD
-    #TODO use UDF
 
     pair = wordCounts.map(lambda x: x[0]) # get the item for the pair (item, count)
     pair = pair.cartesian(pair) # Compute all the pairs of items possible
@@ -71,13 +55,3 @@ if sys.argv[1] == "Frequent":
 else:    
     frequent(0.05)
 
-
-
-
-
-#df = spark.read.format("csv").option("delimiter", "\t").load("datas/T10I4D100K.dat").withColumnRenamed("_c0","Basket")
-#df.show(truncate=100)
-
-#tokenizer = Tokenizer(inputCol="Basket", outputCol="Purchase")
-#df = tokenizer.transform(df)
-#print(df.agg(countDistinct("Purchase")))
