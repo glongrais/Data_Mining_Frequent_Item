188d26f55db25dc7790fda9aa4c641b16e44be12
DarkManPC <guillaume.longrais@orange.fr>
2020-11-12 22:20:42 +0100

    opti

--- Main.py
+++ Main.py
@@ -1,56 +1,57 @@
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
+from pyspark.conf import SparkConf
 from pyspark.ml.feature import Tokenizer
 from pyspark.sql.functions import udf
 from pyspark.sql.types import ArrayType, IntegerType
+from itertools import *
 import sys
+import gc
 
+from operator import truediv
 
-sc = SparkContext('local[*]')
+conf = SparkConf().setAppName("App")
+conf = (conf.setMaster('local[*]')
+       .set('spark.driver.memory', '8G')
+       .set("spark.driver.host", "127.0.0.1"))
+
+sc = SparkContext(conf = conf)
+#print(sc._conf.get('spark.driver.memory'))
 spark = SparkSession(sc)
 
 def frequent(support):
     rawPurchases = sc.textFile("datas/T10I4D100K.dat")
     total = rawPurchases.count()
+
     words = rawPurchases.flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b: a + b)
     wordCounts = wordCounts.filter(lambda x: len(x[0])>=1 ) #Remove retour à la ligne
     wordCounts = wordCounts.filter(lambda x: (x[1]/total)*100 >= support ) #Remove retour à la ligne
 
-    pair = wordCounts.map(lambda x: x[0]) # get the item for the pair (item, count)
-    pair = pair.cartesian(pair) # Compute all the pairs of items possible
-    pair = pair.filter(lambda x: x[0] != x[1]) # Remove pairs (x, x)
-    pair = pair.map(lambda x: x if x[0] < x[1] else (x[1], x[0])) # Order elements in pair  : [(1,2), (2,1)] -> [(1,2), (1,2)]
-    pair = pair.distinct() # Remove duplicated pairs
+    wordsFreq = wordCounts.map(lambda x: x[0]) # get the item for the pair (item, count)
+    pair = sc.parallelize(list(combinations(wordsFreq.collect(), 2))) # Compute all the pairs of items possible
 
     pairDatas = pair.collect()
+    dataSet = set(pairDatas)
+    wordsFreqList = wordsFreq.collect()
 
     purchases = rawPurchases.map(lambda x: x.split(" "))
 
-    pairCount = sc.emptyRDD()
+    pairCount = purchases.filter(lambda x: len(list(set(x) & set(wordsFreqList))) >= 2)
+    #print("step 1 : "+str(pairCount.count()))
+    pairCount = pairCount.map(lambda x: list(combinations(x, 2)))
+    #print("step 2 : "+str(pairCount.count()))
+    pairCount = pairCount.map(lambda x: list(set(x) & dataSet))
+    #print("step 3 : "+str(pairCount.count()))
+    pairCount = pairCount.flatMap(lambda x: x)
+    #print("step 4 : "+str(pairCount.count()))
+    #pairCount = pairCount.filter(lambda x: x in pairDatas)
+    #print("step 4 : "+str(pairCount.count()))
+    pairCount = pairCount.map(lambda pair: (pair, 1)).reduceByKey(lambda a,b: a + b)
+    #print("step 5 : "+str(pairCount.count()))
+    pairCount = pairCount.filter(lambda x: (x[1]/total)*100 >= support) 
 
-    datas = purchases.collect()
-
-    index = 0
-    for i in datas:
-        print(index)
-        tmp = sc.parallelize(i)
-        tmp = tmp.cartesian(tmp)
-        tmp = tmp.filter(lambda x: x in pairDatas)
-        tmp = tmp.map(lambda pair: (pair, 1)).reduceByKey(lambda a,b: a + b)
-        pairCount = pairCount.union(tmp)
-        index +=1
-
-    pairCount = pairCount.reduceByKey(lambda a,b: a + b)
-    pairCount = pairCount.filter(lambda x: (x[1]/total)*100 >= support )
-
-    print(wordCounts.collect())
-    print(pair.collect())
-    print(pairCount.collect())
-
-
-
-
+    print(pairCount.count())
 
 
 if sys.argv[1] == "Frequent":
