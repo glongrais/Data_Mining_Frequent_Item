07ad139f09276354c5f4667cf2959ac009832a81
DarkManPC <guillaume.longrais@orange.fr>
2020-11-13 19:53:11 +0100

    opti

--- Main.py
+++ Main.py
@@ -24,34 +24,57 @@ def frequent(support):
     total = rawPurchases.count()
 
     words = rawPurchases.flatMap(lambda line: line.split(" "))
+    purchases = rawPurchases.map(lambda x: x.split(" "))
+
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b: a + b)
     wordCounts = wordCounts.filter(lambda x: len(x[0])>=1 ) #Remove retour à la ligne
     wordCounts = wordCounts.filter(lambda x: (x[1]/total)*100 >= support ) #Remove retour à la ligne
 
-    wordsFreq = wordCounts.map(lambda x: x[0]) # get the item for the pair (item, count)
-    pair = sc.parallelize(list(combinations(wordsFreq.collect(), 2))) # Compute all the pairs of items possible
+    wordsFreq = wordCounts.sortByKey(True)
+    wordsFreq = wordsFreq.map(lambda x: x[0]) # get the item for the pair (item, count)
+    # pair = sc.parallelize(list(combinations(wordsFreq.collect(), 2))) # Compute all the pairs of items possible
 
-    pairDatas = pair.collect()
-    dataSet = set(pairDatas)
-    wordsFreqList = wordsFreq.collect()
+    # pairDatas = pair.collect()
+    # dataSet = set(pairDatas)
+    # wordsFreqList = wordsFreq.collect()
+    # wordsFreqSet = set(wordsFreqList)
 
-    purchases = rawPurchases.map(lambda x: x.split(" "))
+    # pairCount = purchases.filter(lambda x: len(list(set(x) & wordsFreqSet)) >= 2)
+    # pairCount = pairCount.map(lambda x: list(combinations(x, 2)))
+    # pairCount = pairCount.map(lambda x: list(set(x) & dataSet))
+    # pairCount = pairCount.flatMap(lambda x: x)
+    # pairCount = pairCount.map(lambda pair: (pair, 1)).reduceByKey(lambda a,b: a + b)
+    # pairCount = pairCount.filter(lambda x: (x[1]/total)*100 >= support) 
 
-    pairCount = purchases.filter(lambda x: len(list(set(x) & set(wordsFreqList))) >= 2)
-    #print("step 1 : "+str(pairCount.count()))
-    pairCount = pairCount.map(lambda x: list(combinations(x, 2)))
-    #print("step 2 : "+str(pairCount.count()))
-    pairCount = pairCount.map(lambda x: list(set(x) & dataSet))
-    #print("step 3 : "+str(pairCount.count()))
-    pairCount = pairCount.flatMap(lambda x: x)
-    #print("step 4 : "+str(pairCount.count()))
-    #pairCount = pairCount.filter(lambda x: x in pairDatas)
-    #print("step 4 : "+str(pairCount.count()))
-    pairCount = pairCount.map(lambda pair: (pair, 1)).reduceByKey(lambda a,b: a + b)
-    #print("step 5 : "+str(pairCount.count()))
-    pairCount = pairCount.filter(lambda x: (x[1]/total)*100 >= support) 
+    # print(pairCount.count())
 
-    print(pairCount.count())
+    tupleCount = wordCounts
+    index = 2
+
+    combinations(wordsFreq.collect(), 4)
+
+    while tupleCount.count() > 0:
+        print(index)
+        tupleFreq = tupleCount.map(lambda x: x[0]) # get the item for the pair (item, count)
+        tupleFreqSet = set(tupleFreq.collect())
+        tuplePrec = sc.parallelize(list(combinations(wordsFreq.collect(), index))) # Compute all the pairs of items possible
+        if index > 2: 
+            tuplePrec = tuplePrec.filter(lambda x: list(set(combinations(list(x), (index - 1))) & tupleFreqSet))
+
+        tuplePrecDatas = tuplePrec.collect()
+        dataSet = set(tuplePrecDatas)
+        wordsFreqList = wordsFreq.collect()
+        wordsFreqSet = set(wordsFreqList)
+
+        tupleCount = purchases.filter(lambda x: len(list(set(x) & wordsFreqSet)) >= index)
+        tupleCount = tupleCount.map(lambda x: list(combinations(x, index)))
+        tupleCount = tupleCount.map(lambda x: list(set(x) & dataSet))
+        tupleCount = tupleCount.flatMap(lambda x: x)
+        tupleCount = tupleCount.map(lambda pair: (pair, 1)).reduceByKey(lambda a,b: a + b)
+        tupleCount = tupleCount.filter(lambda x: (x[1]/total)*100 >= support) 
+
+        index += 1
+
 
 
 if sys.argv[1] == "Frequent":
